ee3e577e3f632ffe07d18fd82097c26c8286638c
David Reyes <davidreyesd13@gmail.com>
2020-05-08 15:21:14 -0600

    clases mas login

--- src/lab2_davidreyes/Lab2_DavidReyes.java
+++ src/lab2_davidreyes/Lab2_DavidReyes.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2_davidreyes;
 
-/**
- *
- * @author david
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab2_DavidReyes {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        ArrayList <Casas> lista = new ArrayList();
     }
     
 }
